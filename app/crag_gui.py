f5f71afc01f0462c71e1b3b3af75e4e4987a186f
Engstrom, Frederick <fengstrom@mitre.org>
2020-05-04 02:09:03 -0500

    more restructuring and scanning

--- app/crag_gui.py
+++ app/crag_gui.py
@@ -1,7 +1,12 @@
+import json
+import logging
+from aiohttp import web
 from aiohttp_jinja2 import template
 
 from app.service.auth_svc import check_authorization
 from app.utility.base_world import BaseWorld
+
+from plugins.crag.app.crag_svc import CragService
 
 
 class CragGui(BaseWorld):
@@ -9,9 +14,40 @@ class CragGui(BaseWorld):
     def __init__(self, services, nmap_installed):
         self.services = services
         self.auth_svc = services.get('auth_svc')
-        self.nmap_installed = nmap_installed
+        self.nmap_installed = 1 if nmap_installed else 0
+        self.crag_svc = CragService(services)
+        self.log = logging.getLogger('crag_gui')
 
     @check_authorization
     @template('crag.html')
     async def splash(self, request):
         return dict(nmap=self.nmap_installed, input_parsers=[dict(name='nmap'), dict(name='nessus'), dict(name='siesta')])
+
+    @check_authorization
+    async def crag_core(self, request):
+        try:
+            data = dict(await request.json())
+            index = data.pop('index')
+            options = dict(
+                DELETE=dict(),
+                PUT=dict(),
+                POST=dict(
+                    scan=lambda d: self.scan(),
+                    import_scan=lambda d: self.import_scan(d)
+                )
+            )
+            if index not in options[request.method]:
+                return web.HTTPBadRequest(text='index: %s is not a valid index for the crag plugin' % index)
+            return web.json_response(await options[request.method][index](data))
+        except Exception as e:
+            self.log.error(repr(e), exc_info=True)
+
+    async def scan(self):
+        return dict(output=json.dumps(await self.crag_svc.scan_network(), indent=4))
+
+    async def import_scan(self, data):
+        self.log.debug(json.dumps(data))
+        scan_type = data.get('format')
+        report = data.get('file')
+        return dict(output=await self.crag_svc.import_scan(scan_type, report))
+
--- static/js/crag.js
+++ static/js/crag.js
@@ -9,11 +9,38 @@ function validateNmapInstalled(state){
 }
 
 function validateParser(){
-    validateFormState(true, '#startImport')
+    validateFormState(true, '#startImport');
 }
 
 function startScan(){
+    restRequest('POST', {'index':'scan', 'network':'local'}, displayOutput, '/plugin/crag/api');
 }
 
 function importScan(){
+    $('#file-input').trigger('click');
+}
+
+function uploadScan(file){
+//    let file = $('#file-input').files[0];
+//    let file = document.getElementById('file-input').files[0];
+    let data = {'index': 'import_scan',
+                'format': $('#scan-input-format').val(),
+                'file': file
+                }
+    restRequest('POST', data, displayOutput, '/plugin/crag/api');
+}
+
+$('#file-input').on('change', function (event){
+        if(event.currentTarget) {
+            let filename = event.currentTarget.files[0].name;
+            if(filename){
+                uploadScan(event.currentTarget.files[0]);
+            }
+        }
+    });
+
+
+function displayOutput(data){
+    let results = data;
+    document.getElementById("crag-log").value += results.output + '\n'
 }
