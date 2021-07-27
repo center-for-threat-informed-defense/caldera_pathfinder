90277aed8c1dd048fe2f229d4af63758a534fb67
Chris Jellen <cjellen@mitre.org>
2021-07-27 09:25:17 -0700

    Pathfinder GUI revisions in light of new objects.

--- app/objects/secondclass/c_host.py
+++ app/objects/secondclass/c_host.py
@@ -2,7 +2,8 @@ import marshmallow as ma
 
 from app.utility.base_object import BaseObject
 from plugins.pathfinder.app.objects.secondclass.c_port import PortSchema
-
+from plugins.pathfinder.app.objects.secondclass.c_os import OSSchema
+from plugins.pathfinder.app.objects.secondclass.c_service import ServiceSchema
 
 class HostSchema(ma.Schema):
 
@@ -10,6 +11,9 @@ class HostSchema(ma.Schema):
     ip = ma.fields.String()
     ports = ma.fields.Dict(keys=ma.fields.Integer(), values=ma.fields.Nested(PortSchema()))
     cves = ma.fields.List(ma.fields.String())
+    software = ma.fields.List(ma.fields.Nested(ServiceSchema()))
+    os = ma.fields.Nested(OSSchema())
+    mac = ma.fields.String()
 
     @ma.post_load()
     def build_host(self, data, **_):
@@ -20,9 +24,12 @@ class Host(BaseObject):
 
     schema = HostSchema()
 
-    def __init__(self, ip, hostname=None, ports=None, cves=None, match='.*'):
+    def __init__(self, ip, hostname=None, ports=None, cves=None, software=None, os=None, mac=None, match='.*'):
         super().__init__()
         self.hostname = hostname
         self.ip = ip
         self.ports = ports or dict()
         self.cves = cves or []
+        self.software = software or []
+        self.os = os
+        self.mac = mac
