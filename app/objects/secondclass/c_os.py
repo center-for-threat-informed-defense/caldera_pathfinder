bcc8a3327009210946bb1a45ba411c5e311b6d60
Kyle Heaton <heatonk@purdue.edu>
2021-08-13 11:13:35 -0400

    Change parameter name for PEP8 compliance

--- app/objects/secondclass/c_os.py
+++ app/objects/secondclass/c_os.py
@@ -17,9 +17,9 @@ class OS(BaseObject):
 
     schema = OSSchema()
 
-    def __init__(self, ostype, subtype=None, notes=None, match='.*'):
+    def __init__(self, os_type, subtype=None, notes=None, match='.*'):
         super().__init__()
-        self.osType = ostype
+        self.osType = os_type
         self.subtype = subtype
         self.notes = notes
 
