12e1bc1f40a52d2a5937fa9540b76f95deaa93ba
Kyle Heaton <heatonk@purdue.edu>
2021-08-13 11:23:38 -0400

    Change parameter name for PEP8 compliance

--- app/objects/secondclass/c_service.py
+++ app/objects/secondclass/c_service.py
@@ -5,7 +5,7 @@ from app.utility.base_object import BaseObject
 
 class ServiceSchema(ma.Schema):
 
-    serviceType = ma.fields.String()
+    service_type = ma.fields.String()
     subtype = ma.fields.String()
     notes = ma.fields.String()
 
@@ -20,6 +20,6 @@ class Service(BaseObject):
 
     def __init__(self, service_type, subtype=None, notes=None, match='.*'):
         super().__init__()
-        self.serviceType = service_type
+        self.service_type = service_type
         self.subtype = subtype
         self.notes = notes
