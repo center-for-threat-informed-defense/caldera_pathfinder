c6d8556fc8b418898754fd6db8d2125e4936a5ce
Chris Jellen <cjellen@mitre.org>
2021-12-07 09:50:32 -0800

    VIRTS-3327

--- app/objects/secondclass/c_host.py
+++ app/objects/secondclass/c_host.py
@@ -1,9 +1,17 @@
+from enum import Enum
 import marshmallow as ma
 
 from app.utility.base_object import BaseObject
 from plugins.pathfinder.app.objects.secondclass.c_port import PortSchema
 from plugins.pathfinder.app.objects.secondclass.c_os import OSSchema
 from plugins.pathfinder.app.objects.secondclass.c_service import ServiceSchema
+
+
+class HostAccess(Enum):
+    deny = -1
+    standard = 0
+    allow = 1
+
 
 class HostSchema(ma.Schema):
 
@@ -14,6 +22,10 @@ class HostSchema(ma.Schema):
     software = ma.fields.List(ma.fields.Nested(ServiceSchema()))
     os = ma.fields.Nested(OSSchema())
     mac = ma.fields.String()
+    freebie_abilities = ma.fields.List(ma.fields.String())
+    denied_abilities = ma.fields.List(ma.fields.String())
+    access = ma.fields.Integer()
+    access_prob = ma.fields.Dict(keys=ma.fields.String(), values=ma.fields.Float())
 
     @ma.post_load()
     def build_host(self, data, **_):
@@ -24,7 +36,8 @@ class Host(BaseObject):
 
     schema = HostSchema()
 
-    def __init__(self, ip, hostname=None, ports=None, cves=None, software=None, os=None, mac=None, match='.*'):
+    def __init__(self, ip, hostname=None, ports=None, cves=None, software=None, os=None, mac=None,
+                 freebie_abilities=None, denied_abilities=None, access=None, access_prob=None, match='.*'):
         super().__init__()
         self.hostname = hostname
         self.ip = ip
@@ -33,3 +46,7 @@ class Host(BaseObject):
         self.software = software or []
         self.os = os
         self.mac = mac
+        self.freebie_abilities = freebie_abilities or []
+        self.denied_abilities = denied_abilities or []
+        self.access = access or 0
+        self.access_prob = access_prob or dict()
