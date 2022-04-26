3a9d5d58d2fe2ac7ec974120b2445d3685c6a5da
Kyle Heaton <kheaton@mitre.org>
2022-04-26 14:59:03 -0400

    Getters and setters for v2-algo reports

--- app/objects/c_report.py
+++ app/objects/c_report.py
@@ -60,3 +60,142 @@ class VulnerabilityReport(FirstClassObjectInterface, BaseObject):
         existing.update('hosts', self.hosts)
         existing.update('network_map', self.network_map)
         return existing
+
+    def retrieve_host_by_id(self, id):
+        return self.hosts[id]
+
+    # 
+    # Getters / Setters
+    # 
+
+    def get_host_freebie_abilities(self, id):
+        if id in self.hosts:
+            return self.hosts[id].freebie_abilities
+        return None
+
+    def set_host_freebie_abilities(self, id, abilities):
+        if id in self.hosts:
+            self.hosts[id].freebie_abilities = abilities
+            return True
+        return False
+
+    def get_host_acces(self, id):
+        if id in self.hosts:
+            return self.hosts[id].access
+        return None
+    
+    def set_host_access(self, id, access):
+        if id in self.hosts:
+            self.hosts[id].access = access
+            return True
+        return False
+
+    def get_host_denied_abilities(self, id):
+        if id in self.hosts:
+            return self.hosts[id].denied_abilities
+        return None
+
+    def set_host_denied_abilities(self, id, denied):
+        if id in self.hosts:
+            self.hosts[id].denied_abilities = denied
+            return True
+        return False
+
+    def get_host_ip(self, id):
+        if id in self.hosts:
+            return self.hosts[id].ip
+        return None
+
+    def set_host_ip(self, id, ip):
+        if id in self.hosts:
+            self.hosts[id].ip = ip
+            return True
+        return False
+
+    def get_host_ports(self, id):
+        if id in self.hosts:
+            return self.hosts[id].ports
+        return None
+
+    def set_host_ports(self, id, ports):
+        if id in self.hosts:
+            self.hosts[id].ports = ports
+            return True
+        return False
+
+    def get_host_cves(self, id):
+        if id in self.hosts:
+            return self.hosts[id].cves
+        return None
+
+    def set_host_cves(self, id, cves):
+        if id in self.hosts:
+            self.hosts[id].cves = cves
+            return True
+        return False
+
+    def get_host_software(self, id):
+        if id in self.hosts:
+            return self.hosts[id].software
+        return None
+
+    def set_host_software(self, id, software):
+        if id in self.hosts:
+            self.hosts[id].software = software
+            return True
+        return False
+
+    def get_host_os(self, id):
+        if id in self.hosts:
+            return self.hosts[id].os
+        return None
+
+    def set_host_os(self, id, os):
+        if id in self.hosts:
+            self.hosts[id].os = os
+            return True
+        return False
+    
+    def get_host_hostname(self, id):
+        if id in self.hosts:
+            return self.hosts[id].hostname
+        return None
+
+    def set_host_hostname(self, id, hostname):
+        if id in self.hosts:
+            self.hosts[id].hostname = hostname
+            return True
+        return False
+
+    def get_host_mac(self, id):
+        if id in self.hosts:
+            return self.hosts[id].mac
+        return None
+
+    def set_host_mac(self, id, mac):
+        if id in self.hosts:
+            self.hosts[id].mac = mac
+            return True
+        return False
+
+    def get_host_possible_abilities(self, id):
+        if id in self.hosts:
+            return self.hosts[id].possible_abilities
+        return None
+
+    def set_host_possible_abilities(self, id, possible_abilities):
+        if id in self.hosts:
+            self.hosts[id].possible_abilities = possible_abilities
+            return True
+        return False
+
+    def get_host_access_prob(self, id):
+        if id in self.hosts:
+            return self.hosts[id].access_prob
+        return None
+
+    def set_host_access_prob(self, id, access_prob):
+        if id in self.hosts:
+            self.hosts[id].access_prob = access_prob
+            return True
+        return False
